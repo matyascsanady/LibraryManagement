fa061a04e7e77f6bc70721f022496e8d8aef2cd3
Csanády Mátyás <matyas.csanady@gmail.com>
2025-01-02 20:27:34 +0100

    new function for db creation

--- create_db.py
+++ create_db.py
@@ -1,27 +1,37 @@
 import sqlite3
 
-connection_obj = sqlite3.connect("db/users.db")
-cursor_obj = connection_obj.cursor()
-cursor_obj.execute("DROP TABLE IF EXISTS USERS")
+def create_users_db():
 
-table = """ CREATE TABLE USERS (
-            User_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            User_Name CHAR(50) NOT NULL,
-            Password CHAR(50) NOT NULL
-          ); """
+    connection_obj = sqlite3.connect("db/users.db")
+    cursor_obj = connection_obj.cursor()
+    cursor_obj.execute("DROP TABLE IF EXISTS USERS")
 
-cursor_obj.execute(table)
-print("\"users\" table is created/overridden'")
+    table = """ CREATE TABLE USERS (
+                User_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                User_Name CHAR(50) NOT NULL,
+                Password CHAR(50) NOT NULL
+              ); """
 
-users = [
-    ("Admin", "Admin Password"),
-    ("Librarian", "Library Password"),
-    ("Reader", "Reader Password"),
-]
+    cursor_obj.execute(table)
+    print("\"users\" table is created/overridden'")
 
-cursor_obj.executemany("INSERT INTO USERS (User_Name, Password) VALUES (?, ?)", users)
-print("Initial records inserted.")
+    users = [
+        ("Admin", "Admin Password"),
+        ("Librarian", "Library Password"),
+        ("Reader", "Reader Password"),
+    ]
 
-connection_obj.commit()
+    cursor_obj.executemany("INSERT INTO USERS (User_Name, Password) VALUES (?, ?)", users)
+    print("Initial records inserted.")
 
-connection_obj.close()
+    connection_obj.commit()
+
+    connection_obj.close()
+
+
+def main():
+    create_users_db()
+
+
+if __name__ == "__main__":
+    main()
