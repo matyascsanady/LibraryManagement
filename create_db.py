c1ee1a9c721bfcb0b26c46001449dd7398885640
Csanády Mátyás <matyas.csanady@gmail.com>
2025-01-02 21:43:13 +0100

    books db implemented

--- create_db.py
+++ create_db.py
@@ -16,6 +16,7 @@ def create_users_db():
     cursor_obj.execute(table)
     print("\"users\" table is created/overridden'")
 
+    # Initial users
     users = [
         ("Admin", "Admin Password", "Admin"),
         ("Librarian", "Library Password", "Librarian"),
@@ -27,12 +28,42 @@ def create_users_db():
     print("Initial records inserted.")
 
     connection_obj.commit()
+    connection_obj.close()
 
+
+def create_books_db():
+    connection_obj = sqlite3.connect("db/books.db")
+    cursor_obj = connection_obj.cursor()
+
+    cursor_obj.execute("DROP TABLE IF EXISTS BOOKS")
+
+    table = """ CREATE TABLE BOOKS (
+                Book_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name CHAR(50) NOT NULL,
+                Author CHAR(50) NOT NULL
+              ); """
+
+    cursor_obj.execute(table)
+    print("\"books\" table is created/overridden'")
+
+    # Initial books
+    books = [
+        ("Vörös Lázadás", "Pierce Brown"),
+        ("Arany Háború", "Pierce Brown"),
+        ("Hajnal Csillag", "Pierce Brown"),
+        ("Káosz Évei", "Pierce Brown")
+    ]
+
+    cursor_obj.executemany("INSERT INTO BOOKS (Name, Author) VALUES (?, ?)", books)
+    print("Initial records inserted.")
+
+    connection_obj.commit()
     connection_obj.close()
 
 
 def main():
     create_users_db()
+    create_books_db()
 
 
 if __name__ == "__main__":
